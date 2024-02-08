5cdeacf49bf4a0df4bc6e5ff74920e841954e7ba
Hugh Graham <hugh.a.graham@gmail.com>
2024-02-08 00:29:09 +0000

    new geo json set up for new api

--- R/utils-json-geom.R
+++ R/utils-json-geom.R
@@ -1,24 +1,16 @@
+eng_json_geoms <- function(aoi) {
+  aoi <- wk::wk_coords(aoi) |>
+    dplyr::mutate(dplyr::across(dplyr::everything(), \(x) round(x, 3)))
 
-json_geoms <- function(aoi){
-  paste0('{"geometryType":"esriGeometryPolygon","features":[{"geometry":{"rings":',
-         json_vertices(aoi),
-         ',"spatialReference":{"wkid":27700,"latestWkid":27700}}}],"sr":{"wkid":27700,"latestWkid":27700}}')
-}
+  # Create a list of paired vectors
+  paired_vectors <- mapply(c, aoi$x, aoi$y, SIMPLIFY = FALSE)
 
+  # Install and load the jsonlite package
 
-json_vertices <- function(aoi){
-  verts <- wk::wk_coords(aoi) |>
-    dplyr::mutate(geom=paste0("[", x, ",", y, "]")) |>
-    dplyr::group_by(ring_id) |>
-    dplyr::reframe(geom=paste0( geom, collapse = ",")) |>
-    dplyr::pull(geom)
-
-  vert.leng <- length(verts)
-
-  json_coords <- verts |>
-    sapply( function(x) paste0("[[", x, "]]"), USE.NAMES = FALSE) |>
-    paste(collapse=", ")
-
-  ifelse(vert.leng > 1, paste0("[", json_coords, "]"),
-                         json_coords)
+  # Create a list
+  list(
+    coordinates = list(paired_vectors),
+    type = "Polygon"
+  ) |>
+    jsonlite::toJSON(auto_unbox = TRUE)
 }
